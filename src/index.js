f521414344c77056c58844752a6342bfce4ee245
Erick Lugo <ericklugoj@gmail.com>
2020-12-05 23:25:29 -0600

    empty v1 working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('hola');
+import router from './routes';
+
+window.addEventListener('load', router);
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,26 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  devServer: {
+    // abrir la APP en el BROWSER
+    // open: true,
+    open: 'chrome',
+
+    // HACK para permitir ver la APP desde otros dispositivos
+    // host: '0.0.0.0',
+
+    // ver el progreso de compilacion en la consola
+    progress: true,
+
+    // esto para que todas las URLs que fallen (404) devuelvan nuestro index.html
+    // historyApiFallback: true,
+
+    // para que los errores en consola aparescan en un overlay en el BROWSER
+    // overlay: true,
+
+    // habilitar HMR
+    hot: true,
+  },
   resolve: {
     extensions: ['.js'],
   },
@@ -22,12 +42,10 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebPackPlugin([
-      {
-        inject: true,
-        template: './public/index.html',
-        filename: './index.html',
-      },
-    ]),
+    new HtmlWebPackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html',
+    }),
   ],
 };
